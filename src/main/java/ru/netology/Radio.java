17c6f140eef534b65ab69346830d2c9c97d24251
Grigory <gregkarpov64@gmail.com>
2025-09-09 23:24:27 +0400

    Реализовать гибкую логику станции и объема

--- src/main/java/ru/netology/Radio.java
+++ src/main/java/ru/netology/Radio.java
@@ -1,56 +1,61 @@
 package ru.netology;
 
 public class Radio {
+
     private int currentStation;
     private int currentVolume;
+    private final int stationCount;
+
+    // Конструктор №1: принимает количество станций
+    public Radio(int stationCount) {
+        this.stationCount = stationCount;
+    }
+
+    // Конструктор №2: по умолчанию (без параметров)
+    public Radio() {
+        this.stationCount = 10;
+    }
 
     public int getCurrentStation() {
         return currentStation;
-    }
-
-    public void setCurrentStation(int newCurrentStation) {
-        if (newCurrentStation < 0 || newCurrentStation > 9) {
-            return;
-        }
-        this.currentStation = newCurrentStation;
     }
 
     public int getCurrentVolume() {
         return currentVolume;
     }
 
-    public void setCurrentVolume(int newCurrentVolume) {
-        if (newCurrentVolume < 0 || newCurrentVolume > 100) {
+    public void setCurrentStation(int newStation) {
+        if (newStation < 0 || newStation >= stationCount) {
             return;
         }
-        this.currentVolume = newCurrentVolume;
+        this.currentStation = newStation;
     }
 
     public void next() {
-        if (currentStation == 9) {
-            currentStation = 0;
+        if (currentStation == stationCount - 1) {
+            this.currentStation = 0;
         } else {
-            currentStation++;
+            this.currentStation++;
         }
     }
 
     public void prev() {
         if (currentStation == 0) {
-            currentStation = 9;
+            this.currentStation = stationCount - 1;
         } else {
-            currentStation--;
+            this.currentStation--;
         }
     }
 
     public void increaseVolume() {
         if (currentVolume < 100) {
-            currentVolume++;
+            this.currentVolume++;
         }
     }
 
     public void decreaseVolume() {
         if (currentVolume > 0) {
-            currentVolume--;
+            this.currentVolume--;
         }
     }
 }
